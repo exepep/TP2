ce7263d481f44ff0cf8ba9fdd60f971b631d52c7
exepep <attbadreddine@gmail.com>
2024-04-03 01:29:29 +0200

    first commit

---
+++ src/test/java/org/example/orm/OrmApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example.orm;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@SpringBootTest
+class OrmApplicationTests {}
